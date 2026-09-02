6f83c3dfe75f79815946bd4c23a0b48520118214
vuhuynh2035-alt <vuhuynh2035@gmail.com>
2026-09-02 10:59:27 +0700

    Fix TTS concurrency bugs using playbackId (v2.9.55)

--- patch-karaoke.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('js/app.js', 'utf8');
-
-code = code.replace(/TTSService\.playNextChunk\(\);\s*if\(typeof showToast !== 'undefined'\)/,
-`setTimeout(() => { TTSService.playNextChunk(); }, 150);
-                    if(typeof showToast !== 'undefined')`);
-
-fs.writeFileSync('js/app.js', code);
-console.log(code.includes("setTimeout(() => { TTSService.playNextChunk(); }, 150);") ? "Success" : "Failed");
